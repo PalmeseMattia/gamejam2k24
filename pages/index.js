bb368cf5485f8722b6caf64f20e92b97da311b97
PalmeseMattia <palmesedavidemattia@gmail.com>
2024-01-27 04:11:29 +0100

    clean

--- pages/index.js
+++ pages/index.js
@@ -14,14 +14,6 @@ export default function Home() {
     <main className="text-5xl flex flex-col items-center justify-center w-screen h-screen">
       <div className='flex flex-col items-center justify-center h-screen'>
         <h1 className=''>Iscrizione Giocatori</h1>
-        {/*<ul className='space-y-2'>
-          <li><input value={AppContext.player1} onChange={(e) => AppContext.setPlayer1(e.target.value)}/></li>
-          <li><input value={AppContext.player2} onChange={(e) => AppContext.setPlayer2(e.target.value)}/></li>
-          <li><input value={AppContext.player3} onChange={(e) => AppContext.setPlayer3(e.target.value)}/></li>
-          <li><input value={AppContext.player4} onChange={(e) => AppContext.setPlayer4(e.target.value)}/></li>
-          <li><input value={AppContext.player5} onChange={(e) => AppContext.setPlayer5(e.target.value)}/></li>
-          <li><input value={AppContext.player6} onChange={(e) => AppContext.setPlayer6(e.target.value)}/></li>
-  </ul>*/}
         {players.map((str, index) => (
         <div key={index}>
           <label>
