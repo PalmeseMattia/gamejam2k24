93e462212f49a4672cffda0f9f77b5281bc42f10
LiL-LoPe <117383608+LiL-LoPe@users.noreply.github.com>
2024-02-04 16:50:37 +0100

    commonLayout1.32

--- pages/StoryPages/StoryPage_1/1a.js
+++ pages/StoryPages/StoryPage_1/1a.js
@@ -1,25 +1,24 @@
-// import Link from 'next/link';
-// import RulesButton from '../../../components/RulesButton';
-// import { useEffect } from 'react';
-// import { Howl } from 'howler';
+import Link from 'next/link';
+import RulesButton from '../../../components/RulesButton';
+import { useEffect } from 'react';
+import { Howl } from 'howler';
 import CommonLayout from "@/components/commonLayout";
-
 export default function Intro() {
 
-  // useEffect(() => {
-  //   // Primo file audio
-  //   const sound1 = new Howl({
-  //     src: {music},
-  //     autoplay: true,
-  //     loop: true,
-  //     volume: 0.5,
-  //   });
+  useEffect(() => {
+    // Primo file audio
+    const sound1 = new Howl({
+      src: ['/musiche/Shape-Book-01.wav'],
+      autoplay: true,
+      loop: true,
+      volume: 0.5,
+    });
 
-  //   return () => {
-  //     // Scarica entrambi i suoni quando il componente viene smontato
-  //     sound1.unload();
-  //   };
-  // }, []);
+    return () => {
+      // Scarica entrambi i suoni quando il componente viene smontato
+      sound1.unload();
+    };
+  }, []);
 
     return (
       <CommonLayout titleText='Into' imageComponent={null} mainText='antanisbirigudiloreipsum' buttonCTA='sfoglia'/>
